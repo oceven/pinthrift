4632704cbffbf5493607dc7a7dc1ba0753da58bd
oceven <142824916+oceven@users.noreply.github.com>
2026-04-24 18:43:04 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -10,6 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
 class Listing(Base):
     __tablename__ = "listings"
